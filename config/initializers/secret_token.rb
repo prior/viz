2a2f4a649076665274850dbd5bc9815d5e05a8d4
Prior <prior@cracklabs.com>
2011-07-05 18:38:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Viz::Application.config.secret_token = '200da0871952525e0aa63da507d7a83c26a624f8e42f19dbc502eb55caa88e5b867a845b869b3a1d60631ecedc5040489d315b75143d9b08a9baca2d270b7c6e'
